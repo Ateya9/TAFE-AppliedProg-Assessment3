ea5e6cc84f29892f84cd8a5173195af58026e690
Mark Fish <mark.fish272@gmail.com>
2022-05-31 17:16:21 +0800

    set default estimated level to 0

--- item.py
+++ item.py
@@ -2,7 +2,7 @@ from game_object import GameObject
 
 
 class Item(GameObject):
-    def __init__(self, name: str, description: str, visible: bool, estimated_level: int) -> None:
+    def __init__(self, name: str, description: str, visible: bool, estimated_level: int = 0) -> None:
         super().__init__(name, description)
         self.__visible = visible
         self.estimated_level = estimated_level
