1da31046d984b4249c8c034def10233681b44d8f
Mark Fish <mark.fish272@gmail.com>
2022-05-31 18:26:00 +0800

    Removed current location property from player

--- player.py
+++ player.py
@@ -9,7 +9,6 @@ class Player(Creature):
     def __init__(self, name: str, description: str, level: int, max_hp: int, defence: int, attack: int) -> None:
         super().__init__(name, description, level, max_hp, defence, attack)
         self.inventory: list[Item] = []
-        self.current_location: Location
 
     def attack_target(self, target: GameObject):
         if isinstance(target, NPC):
