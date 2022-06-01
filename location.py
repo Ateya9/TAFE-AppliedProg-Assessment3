a98c28b3d85fac42d198cf9da68b4a026e46dfa2
Mark Fish <20074601@tafe.wa.edu.au>
2022-06-01 17:02:26 +0800

    Added update_visibility and added docstrings

--- location.py
+++ location.py
@@ -16,8 +16,16 @@ class Location(GameObject):
         self.location_south = location_south
         self.location_west = location_west
         self.contents = location_contents
+        self.visible = False
 
     def get_matching_objects(self, target: str = "all") -> list[GameObject]:
+        """
+        Returns a list of GameObjects that match the input string. If no string is specified, returns all GameObjects in
+        this location.
+
+        :param target:
+        :return:
+        """
         if target == "all":
             return self.contents
         result = []
@@ -25,4 +33,16 @@ class Location(GameObject):
             if game_object.name.lower() == target.lower():
                 result.append(game_object)
         return result
+
+    def update_visibility(self):
+        """
+        Sets the visibility of this location and all adjacent locations to True.
+
+        :return:
+        """
+        self.visible = True
+        self.location_north.visible = True
+        self.location_east.visible = True
+        self.location_south.visible = True
+        self.location_west.visible = True
 
