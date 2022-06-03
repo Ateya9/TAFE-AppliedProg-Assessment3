6465acb3021a05643afcf851be28c580ea7eafef
Mark Fish <mark.fish272@gmail.com>
2022-06-03 17:50:49 +0800

    Updated some monster stats and added giant bat

--- hostile_NPCs.py
+++ hostile_NPCs.py
@@ -16,11 +16,12 @@ class HostileNPCs(NPCList):
         self.monster_placeholder = NPC("enemy", "Some sort of hostile creature, it's too far away to see what it is.",
                                        0, 0, 0, 0, True)
         self.add_NPC(NPC("Goblin", "A small runty goblin.", 1, 5, 0, 1, True))
-        self.add_NPC(NPC("Massive Spider", "A massive spider! It's about as big as a dog!", 1, 3, 0, 2, True))
-        self.add_NPC(NPC("Large Goblin", "A fairly large goblin.", 3, 10, 1, 1, True))
+        self.add_NPC(NPC("Massive Spider", "A massive spider! It's about as big as a dog!", 2, 4, 0, 2, True))
+        self.add_NPC(NPC("Large Goblin", "A fairly large goblin.", 3, 6, 1, 1, True))
         self.add_NPC(NPC("Wolf", "A wolf with menacing fangs.", 4, 6, 0, 3, True))
         self.add_NPC(NPC("Ghoul", "A lanky ghoul with sharp claws.", 5, 8, 1, 3, True))
-        self.add_NPC(NPC("Skeleton", "A skeleton, but not quite dead.", 6, 5, 0, 5, True))
+        self.add_NPC(NPC("Skeleton", "A skeleton, but not quite dead.", 6, 6, 0, 5, True))
+        self.add_NPC(NPC("Giant Bat", "A giant bat. It looks angry.", 7, 10, 1, 4, True))
         self.add_NPC(NPC("Bear", "A big 'ol bear.", 8, 15, 3, 6, True))
 
     def get_boss(self) -> NPC:
